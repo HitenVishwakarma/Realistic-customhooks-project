49574ebc15d15ec45b6f7bae67115db6dbff51f1
Hiten Vishwakarma <hiten997764@gmail.com>
2021-09-04 13:55:09 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from "react";
+
+import Tasks from "./components/Tasks/Tasks";
+import NewTask from "./components/NewTask/NewTask";
+import useHttp from "./hooks/useHttp";
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp();
+
+  useEffect(() => {
+    const transformTask = (tasksobj) => {
+      const loadedTasks = [];
+
+      for (const taskKey in tasksobj) {
+        loadedTasks.push({ id: taskKey, text: tasksobj[taskKey].text });
+      }
+
+      setTasks(loadedTasks);
+    };
+    fetchTasks(
+      {
+        url: "https://react-http-a999a-default-rtdb.firebaseio.com/tasks.json",
+      },
+      transformTask
+    );
+  }, [fetchTasks]);
+
+  const taskAddHandler = (task) => {
+    setTasks((prevTasks) => prevTasks.concat(task));
+  };
+
+  return (
+    <React.Fragment>
+      <NewTask onAddTask={taskAddHandler} />
+      <Tasks
+        items={tasks}
+        loading={isLoading}
+        error={error}
+        onFetch={fetchTasks}
+      />
+    </React.Fragment>
+  );
+}
+
+export default App;
